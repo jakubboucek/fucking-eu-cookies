df649eedbe3db5663b95e6675eecc426c33382f8
Jakub Bouček <pan@jakubboucek.cz>
2015-08-18 22:30:44 +0200

    First version

---
+++ source/cookies.lang.cz.js
@@ -0,0 +1,7 @@
+var fucking_eu_localizations = {
+	t:'Tento web používá k poskytování služeb, personalizaci reklam a analýze návštěvnosti soubory cookie. Používáním tohoto webu s tím souhlasíte.',
+	a:'Souhlasím',
+	m:'Vice informací',
+	l:'https://www.google.com/intl/cs/policies/technologies/cookies/'
+};
+// @koala-append "cookies.js"
---
+++ source/cookies.lang.en.js
@@ -0,0 +1,7 @@
+var fucking_eu_localizations = {
+	t:'This website uses cookies to ensure you get the best experience on our website.',
+	a:'Accept',
+	m:'More info',
+	l:'https://www.google.com/policies/technologies/cookies/'
+};
+// @koala-append "cookies.js"
