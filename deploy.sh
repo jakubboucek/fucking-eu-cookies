b9d17d6aa81a027f1665ac583065a3781df9710f
Jakub Bouček <pan@jakubboucek.cz>
2015-09-16 23:13:32 +0200

    Improve deploy shell-script (automated gzip)

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --cache-control "public, max-age=31536000" --delete
+rm ./build/*
+grunt
+find ./build/ -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --cache-control "public, max-age=31536000" --content-encoding gzip --delete
