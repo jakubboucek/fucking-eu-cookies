af28fe3e0f304eca1afd7d77a4596df631da3f21
Jakub Bouček <pan@jakubboucek.cz>
2015-09-20 23:19:42 +0200

    Make cache TTL less aggressive (from 1 Year to 1 Week)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rm ./build/*
+rm -r ./build/*
 grunt
 find ./build/ -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --cache-control "public, max-age=31536000" --content-encoding gzip --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --cache-control "public, max-age=604800" --content-encoding gzip --delete
