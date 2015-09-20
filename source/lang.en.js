ba378112faf65fb16671c543ef95bcaaf0ff113b
Jakub Bouček <pan@jakubboucek.cz>
2015-09-20 20:56:11 +0200

    Improve customisation: - allow modify texts, - allow modify behaviour (prepare for next improves)

--- source/lang.en.js
+++
@@ -1,6 +0,0 @@
-var fucking_eu_localizations = {
-	t:'By using this site you agree to the use of cookies for analytics, personalised content and ads.',
-	a:'Got it',
-	m:'Learn more',
-	l:'https://www.google.com/policies/technologies/cookies/'
-};
