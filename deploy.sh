a43be59f795f334790ccfd9fb86d457900b8137a
Jakub Bouček <pan@jakubboucek.cz>
2015-09-21 10:47:35 +0200

    Preview of favicon deploy (issue: not-compressed icon is still uploaded with Content-Encoding ZIP header)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 rm -r ./build/*
 grunt
-find ./build/ -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
+find ./build/ -regex ".*\.\(js\|html\)" -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
 aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --cache-control "public, max-age=604800" --content-encoding gzip --delete
