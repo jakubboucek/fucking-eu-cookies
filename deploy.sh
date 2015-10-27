9b4d1797c686de19f40a2ec1282f68f9b0c5dd6d
Jakub Bouček <pan@jakubboucek.cz>
2015-10-27 11:05:22 +0100

    Add charset to HTTP header for all JS files (for pages what is encoded on another charset)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 rm -r ./build/*
 grunt
 find ./build/ -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --cache-control "public, max-age=604800" --content-encoding gzip --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude '*' --include "*.js" --cache-control "public, max-age=604800" --content-encoding gzip --content-type="application/javascript;charset=utf-8" --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --exclude "*.js" --cache-control "public, max-age=604800" --content-encoding gzip --delete
