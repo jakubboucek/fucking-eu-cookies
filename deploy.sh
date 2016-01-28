d1d0df17493fc6a2d664a3334ce648544407a979
Jakub Bouček <pan@jakubboucek.cz>
2016-01-28 16:01:07 +0100

    Englare cache TTL to 1M.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 rm -r ./build/*
 grunt
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude '*' --include "*.js" --cache-control "public, max-age=604800" --content-type="application/javascript;charset=utf-8" --delete
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --exclude "*.js" --cache-control "public, max-age=604800" --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude '*' --include "*.js" --cache-control "public, max-age=2592000" --content-type="application/javascript;charset=utf-8" --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --exclude "*.js" --cache-control "public, max-age=2592000" --delete
