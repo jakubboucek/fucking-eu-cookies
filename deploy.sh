20f4f2e546b118839103495cb63386ce63d402e2
Jakub Bouček <pan@jakubboucek.cz>
2018-08-18 00:51:18 +0200

    Deploy script - prevent clear beta

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 rm -r ./build/*
 grunt
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude '*' --include "*.js" --cache-control "public, max-age=600" --content-type="application/javascript;charset=utf-8" --delete
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --exclude "*.js" --cache-control "public, max-age=600" --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude '*' --include "*.js" --exclude "*beta/*" --cache-control "public, max-age=600" --content-type="application/javascript;charset=utf-8" --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --exclude "*beta/*" --exclude "*.js" --cache-control "public, max-age=600" --delete
