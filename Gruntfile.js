4c61a887dffcfa3705f6d0aafe33242c337ca836
Jakub Bouček <pan@jakubboucek.cz>
2018-08-18 06:07:25 +0200

    Codestyle

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,65 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		copy: {
-			styles: {
-				src: ['index.html'],
-				dest: 'build/',
-				flatten: true,
-				expand: true
-			},
-			cz: {
-				src: 'source/cookies.js',
-				dest: 'dist/cz.js',
-				options: {
-					process: function(content, path) {
-						grunt.config.set('l18n', grunt.file.readJSON('source/l18n.cz.json'));
-						return grunt.template.process(content);
-					}
-				}
-			},
-			en: {
-				src: 'source/cookies.js',
-				dest: 'dist/en.js',
-				options: {
-					process: function(content, path) {
-						grunt.config.set('l18n', grunt.file.readJSON('source/l18n.en.json'));
-						return grunt.template.process(content);
-					}
-				}
-			},
-			sk: {
-				src: 'source/cookies.js',
-				dest: 'dist/sk.js',
-				options: {
-					process: function(content, path) {
-						grunt.config.set('l18n', grunt.file.readJSON('source/l18n.sk.json'));
-						return grunt.template.process(content);
-					}
-				}
-			}
-		},
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %>\n *  Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>) */\n'
-			},
-			build: {
-				files: {
-					'build/cz.js': ['dist/cz.js'],
-					'build/en.js': ['dist/en.js'],
-					'build/sk.js': ['dist/sk.js']
-				}
-			}
-		},
-	});
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            styles: {
+                src: ['index.html'],
+                dest: 'build/',
+                flatten: true,
+                expand: true
+            },
+            cz: {
+                src: 'source/cookies.js',
+                dest: 'dist/cz.js',
+                options: {
+                    process: function (content, path) {
+                        grunt.config.set('l18n', grunt.file.readJSON('source/l18n.cz.json'));
+                        return grunt.template.process(content);
+                    }
+                }
+            },
+            en: {
+                src: 'source/cookies.js',
+                dest: 'dist/en.js',
+                options: {
+                    process: function (content, path) {
+                        grunt.config.set('l18n', grunt.file.readJSON('source/l18n.en.json'));
+                        return grunt.template.process(content);
+                    }
+                }
+            },
+            sk: {
+                src: 'source/cookies.js',
+                dest: 'dist/sk.js',
+                options: {
+                    process: function (content, path) {
+                        grunt.config.set('l18n', grunt.file.readJSON('source/l18n.sk.json'));
+                        return grunt.template.process(content);
+                    }
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %>\n *  Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>) */\n'
+            },
+            build: {
+                files: {
+                    'build/cz.js': ['dist/cz.js'],
+                    'build/en.js': ['dist/en.js'],
+                    'build/sk.js': ['dist/sk.js']
+                }
+            }
+        },
+    });
 
-	// Load the plugin that provides the "uglify" task.
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-copy');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-	// Default task(s).
-	grunt.registerTask('default', ['copy','uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['copy', 'uglify']);
 
 };
--- source/cookies.js
+++ source/cookies.js
@@ -1,23 +1,23 @@
-(function(w, d){
+(function (w, d) {
     const identificator = 'fucking-eu-cookies';
 
     const config = <%= JSON.stringify({
-				version: pkg.version,
-				l18n: l18n
-			}, null, '\t') %>;
+        version: pkg.version,
+        l18n: l18n
+    }, null, '\t') %>;
 
-	function shutdownAlert() {
-		d[identificator] = true;
+    function shutdownAlert() {
+        d[identificator] = true;
 
         const message = config.l18n.shutdownNotice + " (" + identificator + " v" + config.version + ")";
 
         if (console) {
-			console.error(message);
-		}
+            console.error(message);
+        }
 
-		throw new Error(message);
-	}
+        throw new Error(message);
+    }
 
-	shutdownAlert();
+    shutdownAlert();
 
 })(window, window.document);
