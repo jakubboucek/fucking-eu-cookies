33a10e5a20c3c682a4228a515c77c6c121da6436
Jakub Bouček <pan@jakubboucek.cz>
2018-08-19 02:17:03 +0200

    Deploy script: Increase cache from 10 minutes to 30 days for non-script files, 1 hour to script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -r ./build/*
 grunt
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude '*' --include "*.js" --exclude "*beta/*" --cache-control "public, max-age=600" --content-type="application/javascript;charset=utf-8" --delete
-aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --exclude "*beta/*" --exclude "*.js" --cache-control "public, max-age=600" --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude '*' --include "*.js" --exclude "*beta/*" --cache-control "public, max-age=3600" --content-type="application/javascript;charset=utf-8" --delete
+aws s3 sync ./build/ s3://fucking-eu-cookies/ --exclude ".gitignore" --exclude "*beta/*" --exclude "*.js" --cache-control "public, max-age=2592000" --delete
