d35699e591b96877e170affb3321ce3ed8382160
Jakub Bouček <pan@jakubboucek.cz>
2018-09-24 21:09:24 +0200

    npm upgrade

--- source/cookies.js
+++ source/cookies.js
@@ -1,23 +1,25 @@
-(function (w, d) {
-    const identificator = 'fucking-eu-cookies';
+(function (w) {
+    var identificator = 'fucking-eu-cookies';
 
-    const config = <%= JSON.stringify({
+    var config = <%= JSON.stringify({
         version: pkg.version,
         l18n: l18n
     }, null, '\t') %>;
 
-    function shutdownAlert() {
-        d[identificator] = true;
+    w[identificator] = {
+        version: config.version,
+    };
 
-        const message = config.l18n.shutdownNotice + " (" + identificator + " v" + config.version + ")";
+    function shutdownAlert() {
+        var message = config.l18n.shutdownNotice + " (" + identificator + " v" + config.version + ")";
 
         if (console) {
             console.error(message);
         }
 
-        throw new Error(message);
+        //throw new Error(message);
     }
 
     shutdownAlert();
 
-})(window, window.document);
+})(window);
